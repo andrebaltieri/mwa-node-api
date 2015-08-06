46caa7c1aeb8dbc17e001b653c0343963bc114ef
Andre Baltieri <andrebaltieri@hotmail.com>
2015-08-06 19:05:23 -0300

    Corrigido problema de CORS

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ var app = express();
 
 //CORS middleware
 var allowCors = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, x-access-token');
 
     next();
 };
